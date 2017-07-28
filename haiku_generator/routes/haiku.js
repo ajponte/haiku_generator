c672d131072aad5e1b403bfa8b6508c9905fbb53
Alan Ponte <alan.ponte@locationlabs.com>
2017-07-28 14:20:45 -0700

    Add stub for method

--- haiku_generator/routes/haiku.js
+++ haiku_generator/routes/haiku.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('generate_haiku', function(req, res) {
-            haiku = generate_haiku(function(res, err) {
-               res.render(haiku);
-            });
+            haiku = generate_haiku()
 });
+
+/**
+ *  Generates a haiku using words from the dictionary.
+ */
+function generate_haiku() {
+   return "foo";
+}
 
 module.exports = router;
